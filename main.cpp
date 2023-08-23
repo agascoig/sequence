26c9e1da23da398c3aa24a2b895595a169b5fa65
Alex Gascoigne <alex.gascoigne@outlook.com>
2023-08-23 11:39:35 -0700

    Update.

--- main.cpp
+++ main.cpp
@@ -5,14 +5,13 @@
 
 int main()
 {
-	Sequence<double> a(5, 1);
-	a[3] = 3;
-	std::cout << "a = " << a << std::endl;
+	Sequence<double> a({ 0, 0, 0, 2, 0, 2, 0, 0 });
+	std::cout << "a = " << a.trim() << std::endl;
 
-	Sequence<double> b(10, 2, -1);
-	std::cout << "b = " << b << std::endl;
+	Sequence<double> b({ 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1 });
+	std::cout << "b = " << b.trim() << std::endl;
 
-	std::cout << "a * b = " << (a * b) << std::endl;
+	std::cout << b + a << std::endl;
 
 	return 0;
 }
